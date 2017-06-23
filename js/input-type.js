bdc7fc7b7bba4282a5d49d6b8eb32d534424c2e1
verdantpath <m_dittmann@yahoo.com>
2017-06-23 13:03:57 -0700

    update js

--- js/input-type.js
+++ js/input-type.js
@@ -4,7 +4,9 @@
   var chk = document.getElementById('showPwd');
 
   addEvent(chk, 'change', function(e) {
+    // e.srcElement is a fallback for older versions of IE
     var target = e.target || e.srcElement;
+    // Try catch statement for errors in older versions of IE when the type property is changed
     try {
       if (target.checked) {
         pwd.type = 'text';
